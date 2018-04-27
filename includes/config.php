5ecc9a53f2b899515a3d74e4983ff00474367cfd
alberto <melchorherrera@gmail.com>
2018-04-27 23:25:24 +0200

    config

--- includes/config.php
+++ includes/config.php
@@ -3,6 +3,6 @@
 define('DB_HOST', 'localhost');
 define('DB_DATABASENAME', 'ecommerce');
 define('DB_USERNAME', 'root');
-define('DB_USERPWD', 'xxxx');
+define('DB_USERPWD', 'prueba');
 define('DB_CHARSET', 'utf8mb4');
 ?>
