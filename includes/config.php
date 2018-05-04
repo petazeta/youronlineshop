569f66d86ae24fa85a463725ea60d92b4256b43b
Alberto <melchorherrera@gmail.com>
2018-05-04 15:19:17 +0200

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+// Database user
+define('DB_HOST', 'host');
+define('DB_DATABASENAME', 'dmname');
+define('DB_USERNAME', 'uname');
+define('DB_USERPWD', 'password');
+define('DB_CHARSET', 'utf8mb4');
+?>
