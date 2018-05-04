f07960765074e68d6e4d2d1f3f4bf059e59f8bf1
alberto <melchorherrera@gmail.com>
2018-05-04 15:27:50 +0200

    config

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database user
-define('DB_HOST', 'host');
-define('DB_DATABASENAME', 'dmname');
-define('DB_USERNAME', 'uname');
-define('DB_USERPWD', 'password');
-define('DB_CHARSET', 'utf8mb4');
-?>
