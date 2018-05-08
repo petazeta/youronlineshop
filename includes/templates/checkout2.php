db2fb30b9c66977dbbfdbddc12300ee7f757e93d
alberto <melchorherrera@gmail.com>
2018-05-08 23:57:19 +0200

    finish order

--- includes/templates/checkout2.php
+++ includes/templates/checkout2.php
@@ -8,9 +8,46 @@
   '>
   </div>
   <div style="width:100%; text-align:center;">
-  <a href="" class="btn">Go to log in page</a>
+  <a href="" class="btn">Finish Order</a>
   <script>
     thisElement.onclick=function(){
+      var addressdata=webuser.getRelationship({"name":"users_addresses"}).children[0];
+      var userdata=webuser.getRelationship({"name":"user_userdata"}).children[0];
+      var result=new NodeMale();
+      result.extra={};
+      myalert.properties.timeout=2000;
+      var minchar=5;
+      function checkInput(data) {
+	for (var key in data.properties) {
+	  if (!data.properties[key] ||
+	  (typeof data.properties[key]=="string" && data.properties[key].length < minchar)) {
+	    result.extra.error=true;
+	    result.extra.errorkey=key;
+	    result.extra.errormsg="Error: Not enought characters at " + result.extra.errorkey;
+	    break;
+	  }
+	}
+      }
+      checkInput(userdata);
+      if (!result.extra.error) checkInput(addressdata);
+      if (!result.extra.error) {
+	function validateEmail(email) {
+	  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	  return re.test(String(email).toLowerCase());
+	}
+	if (!validateEmail(userdata.properties.email)) {
+	  result.extra.error=true;
+	  result.extra.errorkey="email";
+	  result.extra.errormsg="Error: Email not correct";
+	}
+      }
+      if (result.extra.error) {
+	myalert.properties.alertmsg=result.extra.errormsg;
+	myalert.showalert();
+	return false;
+      }
+      myalert.properties.alertmsg="Order done";
+      myalert.showalert();
       webuser.refreshView(document.getElementById("centralcontent"), "includes/templates/loggedindata.php");
       return false;
     };
