7e292f3432e1456e973812041325daec59ed23f8
alberto <melchorherrera@gmail.com>
2018-05-18 21:41:53 +0200

    config

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+// Database user
+define('DB_HOST', 'localhost');
+define('DB_DATABASENAME', 'ecommerce');
+define('DB_USERNAME', 'root');
+define('DB_USERPWD', 'prueba');
+define('DB_CHARSET', 'utf8mb4');
+?>
+
