aa7ee367b84ee9d3375c201577b25be35ec7d121
alberto <melchorherrera@gmail.com>
2018-07-27 12:47:43 +0200

    ie

--- includes/documentparts/catgbox.php
+++ includes/documentparts/catgbox.php
@@ -25,42 +25,7 @@
     </tr>
   </table>
 </template>
-<template id="categorytp">
-  <span style="z-index:1">
-    <a href=""></a>
-    <script>
-      thisNode.getRelationship({name: "itemcategoriesdata"}).loadfromhttp({action: "load my children", language: webuser.extra.language.properties.id}, function(){
-	this.getChild().writeProperty(thisElement);
-	var launcher = new Node();
-	launcher.thisNode = this.getChild();
-	launcher.editElement = thisElement;
-	launcher.btposition="btmiddleleft";
-	launcher.appendThis(thisElement.parentElement, "includes/templates/addbutedit.php");
-	var admnlauncher=new Node();
-	admnlauncher.thisNode=thisNode;
-	admnlauncher.editElement = thisElement;
-	admnlauncher.btposition="btmiddleright";
-	admnlauncher.elementsListPos="vertical";
-	admnlauncher.newNode=thisNode.parentNode.newNode.cloneNode(0, null); // we duplicate it so newNode can be reused
-	admnlauncher.newNode.loadasc(thisNode, 2, "id"); //the parent is not the same
-	admnlauncher.newNode.sort_order=thisNode.sort_order + 1;
-	admnlauncher.appendThis(thisElement.parentElement, "includes/templates/addadmnbuts.php");
-      });
-      thisElement.addEventListener("click", function(event) {
-	event.preventDefault();
-	DomMethods.setActive(thisNode);
-	thisNode.getRelationship().loadfromhttp({action:"load my tree", deepLevel: 2}, function(){
-	  this.newNode=thisNode.parentNode.newNode.cloneNode(0, null); // we duplicate it so newNode can be reused
-	  this.newNode.parentNode=new NodeFemale(); //the parentNode is not the same
-	  this.newNode.parentNode.load(this, 1, 0, null, "id");
-	  this.appendThis(document.getElementById("centralcontent"), "includes/templates/admnlisteners.php");
-	  this.refreshView(document.getElementById("centralcontent"),"includes/templates/catalog.php");
-	});
-	return false;
-      });
-    </script>
-  </span>
-</template>
+
 <script>
 domelementsrootmother.addEventListener(["loadLabels", "changeLanguage"], function(){
   var cartboxtt=this.getChild().getNextChild({name: "labels"}).getNextChild({"name":"middle"}).getNextChild({"name":"ctgbxtt"}).getRelationship({name: "domelementsdata"}).getChild();
@@ -87,7 +52,7 @@ domelementsrootmother.addEventListener(["loadLabels", "changeLanguage"], functio
 	//to set the result in a one column table
 	document.querySelector("#catalogbox .boxbody").appendChild(DomMethods.intoColumns(getTpContent(document.querySelector("#categorytbtp")).querySelector("table").cloneNode(true), document.querySelector("#catalogbox .boxbody"), 1));
       });
-      categoriesMother.refreshChildrenView(document.querySelector("#catalogbox .boxbody"),  document.querySelector("#categorytp"), function(){
+      categoriesMother.refreshChildrenView(document.querySelector("#catalogbox .boxbody"),  "includes/templates/category.php", function(){
       });
     });
   });
