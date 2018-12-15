cffda94ed6097b9b79330712813352d98f0b31c9
alberto <melchorherrera@gmail.com>
2018-12-15 22:19:56 +0100

    error config.sample

--- includes/config.sample.php
+++ includes/config.sample.php
@@ -5,4 +5,7 @@ define('DB_DATABASENAME', 'dbname');
 define('DB_USERNAME', 'username');
 define('DB_USERPWD', 'password');
 define('DB_CHARSET', 'utf8mb4');
+
+// For when we want split databases by prefix
+define('DB_REMOVE_PREFIX', false);
 ?>
