84d8a76b8608bc8e5d27c831f872903af3a590c2
alberto <melchorherrera@gmail.com>
2019-03-27 18:46:06 +0100

    config

---
+++ javascript/config.js
@@ -0,0 +1,11 @@
+var Config={
+  requestFilePath: "request.php", //"request.php"
+  logRequests: false, //false, true sometimes for production
+  defaultEditButtonPosition: "btmiddleright", //"btmiddleright"
+  defaultAdmnsButtonsPosition: "bttopcenter", //"bttopcenter"
+  defaultImg: "noimg.png", //"noimg.png"
+  statsOn: true, //true, false for production
+  onEmptyValueText: null, //null, It will be setled later
+  templatesCacheOn: true, //true, better sometimes false for production
+  loadTemplatesAtOnce: true //true, better sometimes false for production
+}
