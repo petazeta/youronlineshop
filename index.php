1fd73d7ce00d90eb9c4e84b58d7f6ae9c7f5bc02
alberto <melchorherrera@gmail.com>
2019-03-28 00:48:40 +0100

    prefix

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-  require('includes/config.php');
-?>
 <!DOCTYPE html>
   <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
@@ -61,7 +58,13 @@
 	}
 	else {
 	  var mytables=new NodeFemale();
-	  mytables.loadfromhttp({action:"load tables"<?php if (DB_PREFIX) echo ', prefix:"' . DB_PREFIX . '"';?>}, function(){
+	  mytables.loadfromhttp({action:"load tables"
+<?php
+require('includes/config.php');
+$tablePrefix=DB_PREFIX;
+if ($tablePrefix) echo ', prefix:"' . $tablePrefix . '"';
+?>
+	  }, function(){
 	    if (mytables.children.length==0) {
 	      myalert.properties.alertmsg='<p><b>No Database Tables.</p>';
 	      myalert.showalert(null, (function(){throw 'No Database Tables';}));
