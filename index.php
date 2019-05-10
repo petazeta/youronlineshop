f6d3c1c1af396a22c64d1cb46441767020b18b33
Alberto Melchor <melchorherrera@gmail.com>
2019-05-10 10:20:11 +0200

    indestance

--- index.php
+++ index.php
@@ -21,33 +21,33 @@ $tablePrefix=DB_PREFIX;
     <script>
       //history facility
       window.onpopstate = function(event) {
-	if (!event.state) return;
-	var link=document.querySelector("a[href='" + event.state.url + "']");
-	if (link) link.click();
+        if (!event.state) return;
+        var link=document.querySelector("a[href='" + event.state.url + "']");
+        if (link) link.click();
       };
       function loadTemplates(callback) {
-	//We load all the templates at once
-	var tpLoader=new Node();
-	tpLoader.appendThis(document.body, "templates.php", function(){
-	  if (callback) callback();
-	});
+        //We load all the templates at once
+        var tpLoader=new Node();
+        tpLoader.appendThis(document.body, "templates.php", function(){
+          if (callback) callback();
+        });
       }
       function loadLabels(callback) {
-	var myLanguage=webuser.extra.language.properties.id;
-	domelementsrootmother.children[0].getNextChild({name: "labels"}).loadfromhttp({action:"load my tree", language: myLanguage}, function(){
-	  if (!Config.onEmptyValueText) Config.onEmptyValueText=this.getNextChild({name: "not located"}).getNextChild({name: "emptyvallabel"}).getRelationship({name: "domelementsdata"}).children[0].properties.value;
-	  if (callback) callback();
-	});
+        var myLanguage=webuser.extra.language.properties.id;
+        domelementsrootmother.children[0].getNextChild({name: "labels"}).loadfromhttp({action:"load my tree", language: myLanguage}, function(){
+          if (!Config.onEmptyValueText) Config.onEmptyValueText=this.getNextChild({name: "not located"}).getNextChild({name: "emptyvallabel"}).getRelationship({name: "domelementsdata"}).children[0].properties.value;
+          if (callback) callback();
+        });
       }
       var webuser=new user();
       var myalert=new Alert();
       myalert.getTp("templates/alert.php", function(){
-	this.myTp=this.xmlTp;
-	this.properties.alertmsg="<p>Retrieving data ...</p><p>Please wait</p>";
-	this.showalert();
-	if (window.screen.width<700) {
-	  myalert.myContainer.querySelector(".alert").className+=" alerttopleft";
-	}
+        this.myTp=this.xmlTp;
+        this.properties.alertmsg="<p>Retrieving data ...</p><p>Please wait</p>";
+        this.showalert();
+        if (window.screen.width<700) {
+          myalert.myContainer.querySelector(".alert").className+=" alerttopleft";
+        }
       });
 
       //We load the dom elements text that will be included in some parts of the document
@@ -56,78 +56,74 @@ $tablePrefix=DB_PREFIX;
       domelementsrootmother=new NodeFemale();
       //First of all we check that the database connection is ok
       domelementsrootmother.loadfromhttp({action:"check db link"}, function(){
-	if (this.extra && this.extra.error) {
-	  myalert.properties.alertmsg='<p><b>Database Connection Failed</b></p><p>Please check includes/config.php file.</p>';
-	  myalert.showalert(null, (function(){throw 'Database Connection Failed';}));
-	}
-	else {
-	  var mytables=new NodeFemale();
-	  mytables.loadfromhttp({action:"load tables"
-<?php
-if ($tablePrefix) echo ', prefix:"' . $tablePrefix . '"';
-?>
-	  }, function(){
-	    if (mytables.children.length==0) {
-	      myalert.properties.alertmsg='<p><b>No Database Tables.</p>';
-	      myalert.showalert(null, (function(){throw 'No Database Tables';}));
-	    }
-	  });
-	}
-	domelementsrootmother.properties.childtablename="TABLE_DOMELEMENTS";
-	domelementsrootmother.properties.parenttablename="TABLE_DOMELEMENTS";
-	domelementsrootmother.loadfromhttp({action:"load root"}, function(){
-	  domelementsroot=domelementsrootmother.children[0];
-	  domelementsroot.loadfromhttp({action:"load my tree", deepLevel: 2}, function(){
-	    webuser.loadfromhttp({action: "load session", sesname: "user"}, function(){
-	      if (!webuser.extra) webuser.extra={};
-	      if (!webuser.extra.language) {
-		languages=new NodeFemale();
-		languages.properties.childtablename="TABLE_LANGUAGES";
-		languages.properties.parenttablename="TABLE_LANGUAGES";
-		languages.loadfromhttp({action:"load all"}, function(){
-		  var webLanguages=[];
-		  languages.children.forEach(function (child){
-		    webLanguages.push(child.properties.code);
-		  });
-		  for (var i=0; i<window.navigator.languages.length; i++) {
-		    window.navigator.languages[i]=window.navigator.languages[i].replace(/-.+/, "");
-		    if (webLanguages.indexOf(window.navigator.languages[i]) >= 0) {
-		      webuser.extra.language=this.getChild({code: window.navigator.languages[i]});
-		      break;
-		    }
-		  }
-		  if (!webuser.extra.language) webuser.extra.language=this.getChild();
-		  loadLabels(function(){
-		    if (Config.loadTemplatesAtOnce!==false) {
-		      loadTemplates(function(){
-			domelementsrootmother.dispatchEvent("loadLabels");
-			myalert.hidealert();
-		      });
-		    }
-		    else {
-		      domelementsrootmother.dispatchEvent("loadLabels");
-		      myalert.hidealert();
-		    }
-		  });
-		});
-	      }
-	      else {
-		loadLabels(function(){
-		  if (supportsTemplate() && Config.loadTemplatesAtOnce!==false) {
-		    loadTemplates(function(){
-		      domelementsrootmother.dispatchEvent("loadLabels");
-		      myalert.hidealert();
-		    });
-		  }
-		  else {
-		    domelementsrootmother.dispatchEvent("loadLabels");
-		    myalert.hidealert();
-		  }
-		});
-	      }
-	    });
-	  });
-	});
+        if (this.extra && this.extra.error) {
+          myalert.properties.alertmsg='<p><b>Database Connection Failed</b></p><p>Please check includes/config.php file.</p>';
+          myalert.showalert(null, (function(){throw 'Database Connection Failed';}));
+        }
+        else {
+          var mytables=new NodeFemale();
+          mytables.loadfromhttp({action:"load tables"<?php if ($tablePrefix) echo ', prefix:"' . $tablePrefix . '"'; ?>}, function(){
+            if (mytables.children.length==0) {
+              myalert.properties.alertmsg='<p><b>No Database Tables.</p>';
+              myalert.showalert(null, (function(){throw 'No Database Tables';}));
+            }
+          });
+        }
+        domelementsrootmother.properties.childtablename="TABLE_DOMELEMENTS";
+        domelementsrootmother.properties.parenttablename="TABLE_DOMELEMENTS";
+        domelementsrootmother.loadfromhttp({action:"load root"}, function(){
+          domelementsroot=domelementsrootmother.children[0];
+          domelementsroot.loadfromhttp({action:"load my tree", deepLevel: 2}, function(){
+            webuser.loadfromhttp({action: "load session", sesname: "user"}, function(){
+              if (!webuser.extra) webuser.extra={};
+              if (!webuser.extra.language) {
+                languages=new NodeFemale();
+                languages.properties.childtablename="TABLE_LANGUAGES";
+                languages.properties.parenttablename="TABLE_LANGUAGES";
+                languages.loadfromhttp({action:"load all"}, function(){
+                  var webLanguages=[];
+                  languages.children.forEach(function (child){
+                    webLanguages.push(child.properties.code);
+                  });
+                  for (var i=0; i<window.navigator.languages.length; i++) {
+                    window.navigator.languages[i]=window.navigator.languages[i].replace(/-.+/, "");
+                    if (webLanguages.indexOf(window.navigator.languages[i]) >= 0) {
+                      webuser.extra.language=this.getChild({code: window.navigator.languages[i]});
+                      break;
+                    }
+                  }
+                  if (!webuser.extra.language) webuser.extra.language=this.getChild();
+                  loadLabels(function(){
+                    if (Config.loadTemplatesAtOnce!==false) {
+                      loadTemplates(function(){
+                        domelementsrootmother.dispatchEvent("loadLabels");
+                        myalert.hidealert();
+                      });
+                    }
+                    else {
+                      domelementsrootmother.dispatchEvent("loadLabels");
+                      myalert.hidealert();
+                    }
+                  });
+                });
+              }
+              else {
+                loadLabels(function(){
+                  if (supportsTemplate() && Config.loadTemplatesAtOnce!==false) {
+                    loadTemplates(function(){
+                      domelementsrootmother.dispatchEvent("loadLabels");
+                      myalert.hidealert();
+                    });
+                  }
+                  else {
+                    domelementsrootmother.dispatchEvent("loadLabels");
+                    myalert.hidealert();
+                  }
+                });
+              }
+            });
+          });
+        });
       });
     </script>
     <div class="backgroundspace">
--- templates/menu.php
+++ templates/menu.php
@@ -6,82 +6,82 @@
       thisElement.href=url;
       //function to show the openclosebutton
       function showOpenButton() {
-	var openlauncher=new Node();
-	openlauncher.appendThis(thisElement.parentElement.querySelector("[data-id=containeropen]"), "templates/butopen.php", function(){
-	  var openclosebutton=thisElement.parentElement.querySelector("[data-id=containeropen]").querySelector("[data-id=openclose]");
-	  //The on mouse over facility
-	  openclosebutton.style.opacity=0;
-	  thisElement.addEventListener("mouseover", function(ev){
-	    openclosebutton.style.opacity=1;
-	  });
-	  thisElement.addEventListener("mouseout", function(ev){
-	    openclosebutton.style.opacity=0;
-	  });
-	  openclosebutton.addEventListener("mouseover", function(ev){
-	    openclosebutton.style.opacity=1;
-	  });
-	  openclosebutton.addEventListener("mouseout", function(ev){
-	    openclosebutton.style.opacity=0;
-	  });
-	});
+        var openlauncher=new Node();
+        openlauncher.appendThis(thisElement.parentElement.querySelector("[data-id=containeropen]"), "templates/butopen.php", function(){
+          var openclosebutton=thisElement.parentElement.querySelector("[data-id=containeropen]").querySelector("[data-id=openclose]");
+          //The on mouse over facility
+          openclosebutton.style.opacity=0;
+          thisElement.addEventListener("mouseover", function(ev){
+            openclosebutton.style.opacity=1;
+          });
+          thisElement.addEventListener("mouseout", function(ev){
+            openclosebutton.style.opacity=0;
+          });
+          openclosebutton.addEventListener("mouseover", function(ev){
+            openclosebutton.style.opacity=1;
+          });
+          openclosebutton.addEventListener("mouseout", function(ev){
+            openclosebutton.style.opacity=0;
+          });
+        });
       }
       if (thisNode.selected) DomMethods.setActive(thisNode); //restablish the active status after clonning parent rel and when refreshing setSelected
       thisNode.getRelationship("domelementsdata").loadfromhttp({action:"load my children", language: webuser.extra.language.properties.id}, function(){
-	this.getChild().writeProperty(thisElement);
-	var launcher = new Node();
-	launcher.thisNode = this.getChild();
-	launcher.editElement = thisElement;
-	launcher.btposition="btbottomcenter";
-	launcher.visibility="hidden";
-	launcher.appendThis(thisElement.parentElement, "templates/addbutedit.php");
-	var admnlauncher=new Node();
-	admnlauncher.thisNode=thisNode;
-	admnlauncher.editElement = thisElement;
-	admnlauncher.visibility="hidden";
-	admnlauncher.newNode=thisNode.parentNode.newNode.cloneNode(0, null); // we duplicate it so newNode can be reused
-	admnlauncher.newNode.loadasc(thisNode, 2, "id"); //the parent is not the same
-	admnlauncher.newNode.sort_order=thisNode.sort_order + 1;
-	admnlauncher.appendThis(thisElement.parentElement, "templates/addadmnbuts.php");
+        this.getChild().writeProperty(thisElement);
+        var launcher = new Node();
+        launcher.thisNode = this.getChild();
+        launcher.editElement = thisElement;
+        launcher.btposition="btbottomcenter";
+        launcher.visibility="hidden";
+        launcher.appendThis(thisElement.parentElement, "templates/addbutedit.php");
+        var admnlauncher=new Node();
+        admnlauncher.thisNode=thisNode;
+        admnlauncher.editElement = thisElement;
+        admnlauncher.visibility="hidden";
+        admnlauncher.newNode=thisNode.parentNode.newNode.cloneNode(0, null); // we duplicate it so newNode can be reused
+        admnlauncher.newNode.loadasc(thisNode, 2, "id"); //the parent is not the same
+        admnlauncher.newNode.sort_order=thisNode.sort_order + 1;
+        admnlauncher.appendThis(thisElement.parentElement, "templates/addadmnbuts.php");
       });
       thisElement.addEventListener('click', function(event){
-	event.preventDefault();
-	DomMethods.setActive(thisNode);
-	thisNode.getRelationship("domelements").loadfromhttp({action: "load my tree", language: webuser.extra.language.properties.id}, function() {
-	  this.newNode=thisNode.parentNode.newNode.cloneNode(0, null); // we duplicate it so newNode can be reused
-	  this.newNode.parentNode=new NodeFemale();
-	  this.newNode.parentNode.load(this, 1, "id");
-	  var pageframe=getTpContent(document.getElementById("pageframetp")).querySelector("*").cloneNode(true);
-	  document.getElementById("centralcontent").innerHTML="";
-	  document.getElementById("centralcontent").appendChild(pageframe);
-	  this.appendThis(pageframe, "templates/admnlisteners.php");
-	  this.refreshChildrenView(pageframe, "templates/paragraph.php");
-	});
-	if (history.state && history.state.url==url || thisNode.sort_order==1 && history.state==null) return; //we dont grab if it is selected by default after category clicking
-	history.pushState({url:url}, null, url);
+        event.preventDefault();
+        DomMethods.setActive(thisNode);
+        thisNode.getRelationship("domelements").loadfromhttp({action: "load my tree", language: webuser.extra.language.properties.id}, function() {
+          this.newNode=thisNode.parentNode.newNode.cloneNode(0, null); // we duplicate it so newNode can be reused
+          this.newNode.parentNode=new NodeFemale();
+          this.newNode.parentNode.load(this, 1, "id");
+          var pageframe=getTpContent(document.getElementById("pageframetp")).querySelector("*").cloneNode(true);
+          document.getElementById("centralcontent").innerHTML="";
+          document.getElementById("centralcontent").appendChild(pageframe);
+          this.appendThis(pageframe, "templates/admnlisteners.php");
+          this.refreshChildrenView(pageframe, "templates/paragraph.php");
+        });
+        if (history.state && history.state.url==url || thisNode.sort_order==1 && history.state==null) return; //we dont grab if it is selected by default after category clicking
+        history.pushState({url:url}, null, url);
       });
       //we add the edit
       if (webuser.isWebAdmin()) {
-	showOpenButton();
+        showOpenButton();
       }
       //Lets add the log event
       webuser.addEventListener("log",
-	function() {
-	  if (!this.isWebAdmin()) {
-	    //to remove the openbutton when logs after webadmin
-	    var containerOpen=thisElement.parentElement.querySelector("[data-id=containeropen]");
-	    if (containerOpen) {
-	      containerOpen.innerHTML="";
-	    }
-	  }
-	  else {
-	    showOpenButton();
-	  }
-	},
-	"openButton", thisNode
+        function() {
+          if (!this.isWebAdmin()) {
+            //to remove the openbutton when logs after webadmin
+            var containerOpen=thisElement.parentElement.querySelector("[data-id=containeropen]");
+            if (containerOpen) {
+              containerOpen.innerHTML="";
+            }
+          }
+          else {
+            showOpenButton();
+          }
+        },
+        "openButton", thisNode
       );
       //removing the listener when node is deleted
       thisNode.addEventListener("deleteNode", function() {
-	webuser.removeEventListener("log", "openButton", thisNode);
+        webuser.removeEventListener("log", "openButton", thisNode);
       }, "deleteOpenButton");
     </script>
     <div class="btmiddleright" data-id="containeropen"></div>
