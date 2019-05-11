0aaca5447fcad09432040d917bf81976eb2fcb32
Alberto Melchor <melchorherrera@gmail.com>
2019-05-11 17:24:28 +0200

    config bug

--- includes/config.sample.php
+++ includes/config.sample.php
@@ -9,4 +9,7 @@ define('DB_CHARSET', 'utf8mb4');
 // For when we want split databases by prefix
 define('DB_REMOVE_PREFIX', false);
 define('DB_PREFIX', null);
+
+// For when we want to manage sessions so the sessions information is available at the database
+define('DB_SESSIONS', true);
 ?>
