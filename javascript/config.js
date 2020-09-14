789967a10a6559cb1f9f0ce3a28b046111b4e1f3
alberto <melchorherrera@gmail.com>
2020-09-14 16:23:56 +0200

    1.12.3

--- javascript/config.js
+++ javascript/config.js
@@ -1,9 +1,5 @@
-/* Config.configOption=value; */
+/* Config.configOption=value;
+    Check options at default.js
+*/
 
-Config.logRequests = false; //false, true sometimes for production
-Config.importExportOn = false; //true or false if deactivated
-Config.chkt2_On = true; //if false skip address insertion
-Config.chkt3_On = true; //if false skip shipping type selection
-Config.chkt4_On = true; //if false skip payment type selection
-Config.defaultmenu_On = true; //if true click first menu on load
-Config.defaultcat_On = false; //if true click first category on load
+Config.showsubcategory_On=true; //if true expand subcategories
