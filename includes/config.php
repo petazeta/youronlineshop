f1d4be78dbc7eb378c3d80db7d3e73ae820609c9
alberto <melchorherrera@gmail.com>
2020-11-26 10:48:05 +0100

    2.0

--- includes/config.php
+++ includes/config.php
@@ -2,8 +2,10 @@
 // Database config
 define('DB_DATABASENAME', 'dbname');
 define('DB_USERNAME', 'uname');
-define('DB_USERPWD', 'pwd');
+define('DB_USERPWD', 'upwd');
 
 define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
+
+define('THEME', 'retro');
 ?>
