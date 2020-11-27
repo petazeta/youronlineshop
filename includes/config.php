e8c51afc6c9b4fc777aa0626a12f3a3275d06c3b
alberto <melchorherrera@gmail.com>
2020-11-27 17:04:29 +0100

    2.0

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,11 @@
+<!--
+  Config file for database and some other issues like:
+  theme: define('THEME', 'yourtheme');
+-->
 <?php
 // Database config
 define('DB_DATABASENAME', 'dbname');
 define('DB_USERNAME', 'uname');
-define('DB_USERPWD', 'upwd');
-
+define('DB_USERPWD', 'pwd');
 define('DB_HOST', 'localhost');
-define('DB_CHARSET', 'utf8');
-
-define('THEME', 'retro');
 ?>
